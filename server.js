aa30b81e1827dc10f0c6e0a0882b1a9b0ffae726
ssmahim01 <mahimzayn666@gmail.com>
2025-02-26 15:37:40 +0600

    Successfully connected to the MongoDB database

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Call express
 const app = express();
+
+// Use app
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async(req, res) => {
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Get data in the root
+app.get("/", async (req, res) => {
     res.send("NFT Minting server is running...");
 });
 
+// Listen at specific port
 app.listen(port, () => {
     console.log(`NFT server running at port ${port}`);
 });
