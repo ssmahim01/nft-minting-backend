d91ff218037915e311ab82cd9f7a0121cb24d4b7
ssmahim01 <mahimzayn666@gmail.com>
2025-02-26 15:53:55 +0600

    Created GET and POST endpoints for retrieve and store data to the MongoDB database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Call express
@@ -23,6 +23,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("nftDB");
+        const nftCollection = database.collection("nfts");
+
+        // GET NFT data by id
+        app.get("/nft/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const findResult = await nftCollection.findOne(query);
+            if (!findResult) {
+                return res.status(404).send({ message: "NFT Not Found" })
+            }
+
+            res.send(findResult);
+        });
+
+        // Store (POST) NFT data
+        app.post("/nft", async (req, res) => {
+            const nftData = req.body;
+            const insertResult = await nftCollection.insertOne(nftData);
+            if (!insertResult) {
+                return res.status(404).send({ message: "Failed to store the NFT" });
+            }
+
+            res.send(insertResult);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
